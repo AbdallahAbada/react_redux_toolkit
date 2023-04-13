1c1d3bf93a9b468f736b52edb2f3ebbb81d7e859
AbdallahAbada <abdallah.abada.1993@gmail.com>
2023-04-13 22:36:17 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { PostList } from './features/post/PostList';
+import AddPostForm from './features/post/AddPostForm';
 
 export const App = () => {
   return (
     <main className="App">
+      <AddPostForm />
       <PostList />
     </main>
   );
