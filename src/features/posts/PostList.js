ef396127ac0bb760f4ace76dca108032149dd703
AbdallahAbada <abdallah.abada.1993@gmail.com>
2023-04-17 11:28:13 +0200

    add reaction buttons

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 
 import React from 'react'
@@ -19,6 +20,7 @@ export const PostList = () => {
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
+            <ReactionButtons post={post} />
         </article>
     ))
 
